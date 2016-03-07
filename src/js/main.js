d950499d1ecba447b61a24d0e97c5bb125e9a731
Daniel Klein <danhirschklein@gmail.com>
2016-03-07 10:03:21 -0700

    Created working letterGrader Function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 // add scripts
 
 console.log("sanity check!");
+
+function letterGrader(grade) {
+  if (grade > 89 && grade < 101) {
+    return 'A'
+  } else if (grade > 79 && grade < 90) {
+    return 'B'
+  } else if (grade > 69 && grade < 80) {
+    return 'C'
+  } else if (grade > 59 && grade < 70) {
+    return 'D'
+  } else if (grade < 60) {
+    return 'F'
+  }
+}

5d4c0ab7b40a545af0d9f7ca5ba401a8666c4041
Daniel Klein <danhirschklein@gmail.com>
2016-03-07 10:02:34 -0700

    Created Testing for letterGrader function

--- src/test/test.js
+++ src/test/test.js
@@ -7,3 +7,38 @@ describe('Compare Numbers', function() {
     expect(1).to.equal(1);
   });
 });
+
+describe('Letter Grader', function() {
+    it('92 or 98 should equal A', function() {
+    expect(letterGrader(98)).to.equal('A');
+    expect(letterGrader(92)).to.equal('A');
+  });
+});
+
+describe('Letter Grader', function() {
+    it('88 or 82 should equal B', function() {
+    expect(letterGrader(88)).to.equal('B');
+    expect(letterGrader(82)).to.equal('B');
+  });
+});
+
+describe('Letter Grader', function() {
+    it('88 or 82 should equal C', function() {
+    expect(letterGrader(78)).to.equal('C');
+    expect(letterGrader(72)).to.equal('C');
+  });
+});
+
+describe('Letter Grader', function() {
+    it('68 or 62 should equal D', function() {
+    expect(letterGrader(68)).to.equal('D');
+    expect(letterGrader(62)).to.equal('D');
+  });
+});
+
+describe('Letter Grader', function() {
+    it('Less than 60 should equal F', function() {
+    expect(letterGrader(59)).to.equal('F');
+    expect(letterGrader(0)).to.equal('F');
+  });
+});
